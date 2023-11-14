e586ee6025e429b5fe919cd695ead3ed18e96a50
Arina <a.kharlamova@urbi.ae>
2023-11-14 04:43:05 +0300

    chore: fix clone command

--- sourcemeter/analyze.sh
+++ sourcemeter/analyze.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 $@
+git clone $@
 projectName=$(basename "$@" .git) 
+if [[ ! -e $projectName/__init__.py ]]; then
+    touch $projectName/__init__.py
+fi
 ./AnalyzerPython -projectBaseDir:./$projectName -projectName:$projectName -resultsDir:./results -pythonVersion:3 -pythonBinary:python3 -currentDate:latest
 mkdir /results/$projectName
 cp ./results/$projectName/python/latest/*.csv /results/$projectName
--- upload_all.py
+++ upload_all.py
@@ -3,4 +3,4 @@ import requests as r
 
 urls = pd.read_csv('ml_top_repos_links.csv')['link'].values
 for url in urls:
-    r.post("http://0.0.0.0:11002/api/tasks", json={"repo_url": url})
+    r.post("http://158.160.6.157:11002/api/tasks", json={"repo_url": url})
